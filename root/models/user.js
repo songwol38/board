a194ac62edabbd1cea42d01977c228c9ec3198ad
김민지 <128395453+songwol38@users.noreply.github.com>
2024-06-29 01:59:24 +0900

    Update user.js
    주석 변경

--- root/models/user.js
+++ root/models/user.js
@@ -4,13 +4,13 @@ const sequelize = require('D:/github/board/root/config/db');
 const User = sequelize.define('User', {
   id: {
     type: DataTypes.INTEGER,
-    primaryKey: true, //기본키
-    unique: true, //고유 필드
+    primaryKey: true,
+    unique: true,
     autoIncrement: true // 자동 증가 설정
   },
   email: {
     type: DataTypes.STRING,
-    unique: true //고유필드
+    unique: true
   },
   passwd: {
     type: DataTypes.STRING,
