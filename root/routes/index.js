e0c65ea852c09f73aade26d57b80b28b31ebdced
김민지 <128395453+songwol38@users.noreply.github.com>
2024-06-29 01:59:59 +0900

    Update index.js
    주석 변경

--- root/routes/index.js
+++ root/routes/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('D:/github/board/root/models/user'); // 데이터베이스 설정 파일
 
-// GET /users: 모든 사용자 조회
 router.get('/users', async (req, res, next) => {
     try {
-        const users = await User.findAll(); // User 모델에서 모든 사용자 조회
-        res.json(users); // JSON 형태로 사용자 목록 응답
+        const users = await User.findAll();
+        res.json(users);
     } catch (error) {
         next(error); // 에러 처리 미들웨어로 전달
     }
